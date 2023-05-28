a45468b2f1dddfccdf7f15eaa9e53ff9c092f151
Jack N <eugesha88@gmail.com>
2023-05-28 16:15:28 +0300

    Update function inser_date_lessons

--- busingesslogic_data_base.py
+++ busingesslogic_data_base.py
@@ -7,8 +7,7 @@ cursor = connect.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS `timetable` (
 id INTEGER NOT NULL PRIMARY KEY,
-data TEXT NOT NULL,
-time TEXT NOT NULL
+date_&_time INTEGER NOT NULL
 );
 """)
 connect.commit()
@@ -18,19 +17,27 @@ def start():
     cursor = connect.cursor()
 
 def insert_date_lesson(*args):
+    f = args[0] + '.' + args[1]
+    date = datetime.datetime.strptime(f, '%d.%m.%Y.%H.%M')
+    date_uts = datetime.datetime.timestamp(date)
+
     cursor.execute(f"""
-            INSERT INTO `timetable` VALUES
-                ({args[0]}, {args[1]});
+            INSERT INTO `timetable`.`date_&_time` VALUES
+                ({date_uts});
         """)
     connect.commit()
     return "Данные вставлены"
 
 
 def get_date_of_all_lesson():
-    res = cursor.execute("SELECT `data`, `time` FROM `timetable`;")
+    res = cursor.execute("SELECT `date_&_time` FROM `timetable`;")
     lstDateTime = res.fetchall()
     str = ''
 
     for i in lstDateTime:
         str += f"""Занятие по изучению языка Python состоится {i[0]} в {i[1]}.\n"""
     return str
+
+def get_date_nearest_lesson():
+    pass
+
