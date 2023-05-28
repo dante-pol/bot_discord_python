12e4f5b2e50168e10ba1f526fa659634bba6b5b1
Tsukimo1 <brother227_z@mail.ru>
2023-05-28 15:24:38 +0300

    create instrumetal_command

--- main.py
+++ main.py
@@ -5,18 +5,6 @@ from businesslogic import DefaultCommand
 from discord.ext import commands
 
 
-@bot.command() # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
-async def att(ctx): # Создаём функцию и передаём аргумент ctx.
-    author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
-    await ctx.send(f'руддщ')
-
-
-
-@bot.command() # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
-async def addtt(ctx, arg1, arg2): # Создаём функцию и передаём аргумент ctx.
-    author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
-    await ctx.send(f'{arg1} {arg2}')
-
 
 # token = input("input token :")
 # bot.run(token)
